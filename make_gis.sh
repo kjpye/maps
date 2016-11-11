bb21b99ddbff9d02e1b06c183ed3104670e72282
Kevin Pye <Kevin.Pye@gmail.com>
2016-11-11 17:11:04 +1100

    Fewer error messages running scripts for the first time

--- make_gis.sh
+++ make_gis.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/ksh
 
-dropdb map250k
+dropdb --if-exists map250k
 createdb map250k -T template_postgis
-psql map250k -c 'create extension postgis'
-psql map250k -c 'create extension postgis_topology'
+#psql map250k -c 'create extension postgis'
+#psql map250k -c 'create extension postgis_topology'
 psql map250k -f schema.gis250k | grep -v INSERT
 psql map250k -f mksymbols_ga | grep -v INSERT
 psql map250k -f mkdisptable.ga | grep -v INSERT
 
-for mdb in ../../Maps/GA/mdb/?????.mdb
+for mdb in ../../Maps/mdb/?????.mdb
 do
 #  ./add_map.sh $mdb
 echo $mdb
