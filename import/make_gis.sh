0aaa8eb68ae7a0319ef6cde1d38937e92d1d8651
Kevin Pye <admin-Kevin.Pye@cenitex.vic.gov.au>
2019-06-21 10:54:15 +1000

    Allow make_gis.sh to take an argument (database name)

--- import/make_gis.sh
+++ import/make_gis.sh
@@ -1,20 +1,26 @@
 #!/bin/bash -x
 
-dropdb --if-exists map250k
-#createdb map250k -T template_postgis
-createdb map250k
-psql map250k -c 'create extension postgis'
-psql map250k -c 'create extension postgis_topology'
-psql map250k -f schema.gis250k | grep -v INSERT
-psql map250k -f mksymbols_ga | grep -v INSERT
-psql map250k -f mkdisptable.ga | grep -v INSERT
+db=$1
+if [ -s $db ]
+then
+  db=defaultdb
+fi
+
+dropdb --if-exists $db
+#createdb $db -T template_postgis
+createdb $db
+psql $db -c 'create extension postgis'
+psql $db -c 'create extension postgis_topology'
+psql $db -f schema.gis250k | grep -v INSERT
+psql $db -f mksymbols_ga | grep -v INSERT
+psql $db -f mkdisptable.ga | grep -v INSERT
 
 for mdb in ~/maps/GA/Vector_data/*.mdb
 do
 #  ./add_map.sh $mdb
 echo $mdb
-  ./read_mdb.pl $mdb | ./ins250gis.pl --db=map250k
+  ./read_mdb.pl $mdb | ./ins250gis.pl --db=$db
 done
 
-#psql map250k -f tracks.insertions | grep -v INSERT
-psql map250k -c "vacuum analyze;"
+#psql $db -f tracks.insertions | grep -v INSERT
+psql $db -c "vacuum analyze;"
